494953ba7babf64eba583089eb693415fc80375d
Kryvenko Borys <krybordevelop@gmail.com>
2023-01-28 21:53:10 +0200

    change to class

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 import express from 'express'
 import fs from 'fs/promises'
 import http from 'http'
-import get_nginx_data from './src/get_nginx_data.js'
+import { Nginx_data_getter} from './src/get_nginx_data.js'
 import { WebSocketServer } from 'ws'
 
 
 const app = express()
 const server = http.createServer(app);
 const wss = new WebSocketServer({server})
-console.log(get_nginx_data())
 
 app.use(express.static('public'))
 
@@ -16,19 +15,17 @@ app.use(express.static('public'))
 wss.on('connection', (ws) => {
     //console.log('client connected')
     ws.on('message', (message) => {
-        let test
         message = JSON.parse(message.toString())
-        console.log(message)
         if(message.message === "start"){
             let interval = message.interval
-             test = setInterval(async (interval) => {
+            let test = setInterval(async (interval) => {
                 let data = await get_nginx_data()
                 let parsed_data = JSON.stringify(data)
                 ws.send(parsed_data)
             }, interval*1000 )
 
         }else if(message.message === "stop"){
-            clearInterval(test)
+            console.log(test)
         }else{
 
         }
@@ -39,7 +36,9 @@ wss.on('connection', (ws) => {
     //ws.send(data);
 });
 
+
+let mynginx = new Nginx_data_getter("http://127.0.0.1/nginx_status")
+console.log(mynginx.collect_data(20))
 server.listen(process.env.PORT || 8999, () => {
     console.log(`Server started on port ${server.address().port} :)`);
 });
-
--- src/get_nginx_data.js
+++ src/get_nginx_data.js
@@ -1,4 +1,5 @@
 import got from 'got'
+import fs from 'fs/promises'
 
 
 
@@ -16,10 +17,10 @@ const parse_response = (response) => {
    return(final_josn)
 }
 
-const get_nginx_data = async (url='http://127.0.0.1/nginx_status') => {
+export const get_nginx_data = async (url='http://127.0.0.1/nginx_status') => {
     try{
     const {body} = await got(url, {});
-        console.log(parse_response(body))
+        //console.log(parse_response(body))
         return parse_response(body)
     }catch(err){
         console.log(err)
@@ -28,4 +29,76 @@ const get_nginx_data = async (url='http://127.0.0.1/nginx_status') => {
 }
 
 
-export default get_nginx_data
+export class Nginx_data_getter{
+    constructor(nginx_status_url, mode, options){
+        this.nginx_status_url = nginx_status_url
+        this.mode =  mode | 'simple'
+        if(options){
+            this.count_my_requests = options.count_my_requests | false
+            this.interval = options.interval | 60
+        }  
+    }
+
+    static async chek_connection(nginx_status_url) {
+        try{
+            const {body} = await got(nginx_status_url, {});
+                if(body.indexOf("server accepts handled requests") == -1){
+                    console.log(`conection succesfull, but site ${nginx_status_url} does not lock like nginx sub stats`)
+                    return 'conection succesfull, but site does not lock like nginx sub stats'
+                }
+                //console.log(body)
+                return body
+            }catch(err){
+                console.log(`cannot connect to ${nginx_status_url}`)
+                return err.code  
+            }
+    } 
+
+    async get_data(){
+        
+    }
+
+    parse_nginx_response(response){
+        let parse_data = response.split(' ')
+        const final_josn = {}
+        final_josn['Active connections'] = parse_data[2]
+        final_josn['Accepts Total'] = parse_data[7]
+        final_josn['Handled Total'] = parse_data[8]
+        final_josn['Requests Total'] = parse_data[9]
+        final_josn['Reading'] = parse_data[11]
+        final_josn['Writing'] = parse_data[13]
+        final_josn['Waiting'] = parse_data[15]
+        final_josn['Raw Data'] = response
+        return(final_josn)
+     }
+
+     async get_nginx_data(){
+        try{
+            const {body} = await got(this.nginx_status_url, {});
+                return this.parse_nginx_response(body)
+            }catch(err){
+                console.log(err)
+                return err.code  
+            }
+     }
+
+      
+      async collect_data(timer){
+        // get initial values
+        let start = Date.now()
+        let get_start_value = await get_nginx_data()
+
+        setTimeout(async()=>{
+            let get_end_value = await get_nginx_data()
+            let final = {}
+            final.accepts = get_end_value['Accepts Total'] - get_start_value['Accepts Total']
+            final.handelet = get_end_value['Handled Total'] - get_start_value['Handled Total']
+            final.total = get_end_value['Requests Total'] - get_start_value['Requests Total']
+            final.per = timer
+            final.avg = final.total/timer
+            console.log(final)
+            return final
+        },timer*1000)
+      }
+      
+}
