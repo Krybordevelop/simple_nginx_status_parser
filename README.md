5ea3c87d6d606b5bdba20f93461b6bface0acaa6
Kryvenko Borys <krybordevelop@gmail.com>
2023-01-29 02:47:13 +0200

    add cli

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # simple_nginx_status_parser
 nodejs, ws, parser, nginx, practice
+
+Requirements:
+1.
+http://nginx.org/en/docs/http/ngx_http_stub_status_module.html
+2.
+NodeJs
--- src/get_nginx_data.js
+++ src/get_nginx_data.js
@@ -1,22 +1,6 @@
 import got from 'got'
 import fs from 'fs/promises'
 
-
-
-const parse_response = (response) => {
-   let parse_data = response.split(' ')
-   const final_josn = {}
-   final_josn['Active connections'] = parse_data[2]
-   final_josn['Accepts Total'] = parse_data[7]
-   final_josn['Handled Total'] = parse_data[8]
-   final_josn['Requests Total'] = parse_data[9]
-   final_josn['Reading'] = parse_data[11]
-   final_josn['Writing'] = parse_data[13]
-   final_josn['Waiting'] = parse_data[15]
-   final_josn['Raw Data'] = response
-   return(final_josn)
-}
-
 export const get_nginx_data = async (url='http://127.0.0.1/nginx_status') => {
     try{
     const {body} = await got(url, {});
@@ -100,5 +84,5 @@ export class Nginx_data_getter{
             return final
         },timer*1000)
       }
-      
+
 }
